44b0e8da439c8dc380513d091db52b8ad924760d
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-01-19 16:47:31 -0300

    feat rotas e nav no app

--- minha-biblioteca/src/App.tsx
+++ minha-biblioteca/src/App.tsx
@@ -1,10 +1,20 @@
-import { Search } from "./pages/Search"
+import { Search } from "./pages/Search";
+import { Home } from "./pages/Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Search />
-    </>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |
+          <Link to="/search">Procurar</Link>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </nav>
+      </div>
   )
 }
 
