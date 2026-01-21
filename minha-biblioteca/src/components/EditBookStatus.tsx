eade5481aa6e8c626f80fcb59a4e33f22c49d079
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-01-20 23:54:31 -0300

    feat status editavel no BookCard

---
+++ minha-biblioteca/src/components/EditBookStatus.tsx
@@ -0,0 +1,52 @@
+import { useContext, useState } from "react";
+import { BooksContext  } from "../contexts/BooksContext";
+import { BookStatusText, BookStatusValue } from "../types/Status";
+import { BookStatus } from "../types/Book";
+
+interface EditBookStatusProp {
+    status: BookStatus;
+    id: string;
+}
+
+export function EditBookStatus({ status, id }: EditBookStatusProp) {
+    const statusText = BookStatusText[status]
+    const {updateStatus} = useContext(BooksContext)
+
+    const [editing, setEditing] = useState(false);
+    const [tempStatus, setTempStatus] = useState<BookStatus>(status);
+
+    function startEdit() {
+        setTempStatus(status);
+        setEditing(true);
+    }
+
+    function finishEdit() {
+        
+
+        setEditing(false);
+        updateStatus(id, tempStatus)
+    }
+
+    return (
+        <span>
+            {editing ? (
+                <>
+                    <label>Status:</label>
+                    <select 
+                        value={tempStatus} 
+                        onChange={(e) => setTempStatus(e.target.value as BookStatus)}
+                        onBlur={finishEdit}
+                    >
+                        <option value={BookStatusValue.LIDO}>{BookStatusText.LIDO}</option>
+                        <option value={BookStatusValue.LENDO}>{BookStatusText.LENDO}</option>
+                        <option value={BookStatusValue.QUERO_LER}>{BookStatusText.QUERO_LER}</option>
+                    </select>
+                </>
+            ) : (
+                <p onClick={startEdit}>
+                    {statusText}
+                </p>
+            )}
+        </span>
+    )
+}
